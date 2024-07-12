67b12f8fcb36b9fa794633bd88676d310d3a2c98
Ibsham <progwithibi@gmail.com>
2024-07-13 01:13:38 +0500

    passport authentication flow completed

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    "856570693543-7tqad3vvb2laghm07f24p2irdjl9ihqu.apps.googleusercontent.com",
+  googleClientSecret: "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",
+};
+
+/*
+http://localhost:5000/auth/google/callback?
+
+*/
